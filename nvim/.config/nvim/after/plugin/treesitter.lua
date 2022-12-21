85ebfc68943b8189b94d7335cb70a8295b9819ad
Mario Sarcevic <mario.sarcevic.985@gmail.com>
2022-12-21 09:26:34 +0100

    Add nvim-ts-autotag

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -18,6 +18,10 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+  },
+  -- Requires windwp/nvim-ts-autotag - Automatically close html tags
+  autotag = {
+    enable = true,
   }
 }
 
--- nvim/.config/nvim/lua/marshar/packer.lua
+++ nvim/.config/nvim/lua/marshar/packer.lua
@@ -47,5 +47,6 @@ return require('packer').startup(function(use)
   }
 
   use('sbdchd/neoformat')
+  use('windwp/nvim-ts-autotag')
 end)
 
