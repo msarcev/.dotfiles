71097b747cfe2094034dbc4ab861f5ed4a013ce9
Mario Sarcevic <mario.sarcevic.985@gmail.com>
2025-07-02 21:49:14 +0200

    command for search

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -8,6 +8,18 @@ vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>s', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set( 'n', '<leader>ps', '<cmd>lua require(\'telescope.builtin\').grep_string({search = vim.fn.expand("<cword>")})<cr>', {})
+
+-- Visual mode: search for selected text
+vim.keymap.set('v', '<leader>rg', '"zy:Telescope grep_string search=<C-r>z<CR>', {})
+
+-- Telescope search word under cursor
+vim.keymap.set("n", "<leader>rw", function()
+    local word = vim.fn.expand("<cword>")
+    if word == "" then
+        print("No word under cursor")
+        return
+    end
+    require('telescope.builtin').grep_string({ search = word })
+end, { desc = "Telescope search word under cursor" })
 
 
